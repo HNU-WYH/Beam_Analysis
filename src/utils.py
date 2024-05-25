d37e4e0d09f0d5de8414dc1a709e89635556d9cf
HNU-WYH <1178736956@qq.com>
2024-05-25 17:16:38 +0200

    update the newmark method and test file

--- src/utils.py
+++ src/utils.py
@@ -1,37 +1,48 @@
 import numpy as np
 
 
-class NewMarkBeta:
+class NewMark:
     '''
     solving $M\ddot x + S x = f$
     '''
 
-    def __inti__(self, tau: float, num_steps: int, beta=0.25, gamma=0.5):
+    def __init__(self, tau: float, num_steps: int, beta=0.25, gamma=0.5):
         self.beta = beta
         self.gamma = gamma
         self.tau = tau
         self.num_steps = num_steps
 
-    def solve(self, M: np.ndarray, S: np.ndarray, f: np.ndarray, x: np.ndarray, ddx: np.ndarray):
+    def solve(self, M: np.ndarray, S: np.ndarray, f: np.ndarray, x: np.ndarray,dx: np.ndarray, ddx: np.ndarray):
         '''
         M: the Mass Matrix
         S: the Stiffness Matrix
+        f: the external force varies with time
         x: initial condition of x\in\mathbb R^n
+        dx: intial condition of \dot x\in\mathbb R^n
         ddx: intial condition of \ddot x\in\mathbb R^n
+
+        Newmark Method:
+        solve the equation of $(S\beta h_j^2)\ddot{x}_{j+1}=f-M\ddot{x}_j-Sx_j^*$
+        A = S\beta h_j^2
+        sol = ddx_{j+1}
+        b =f-M\ddot{x}_j-Sx_j^*
         '''
-        u = np.zeros([x.shape, self.num_steps])
-        ddu = np.zeros([ddx.shape, self.num_steps])
-        ddu[:, 0] = ddu
+        # Initialize
+        num_dofs = x.shape[0]
+        u = np.zeros([num_dofs, self.num_steps])
+        du = np.zeros([num_dofs, self.num_steps])
+        ddu = np.zeros([num_dofs, self.num_steps])
+
         u[:, 0] = x
+        du[:, 0] = dx
+        ddu[:, 0] = ddx
 
         for j in range(self.num_steps - 1):
-            u_star = u[:, j] + (0.5 - self.beta) * self.tau ** 2 * ddu[:, j]
-            # solve the equation of $(S\beta h_j^2)\ddot{x}_{j+1}=f-M\ddot{x}_j-Sx_j^*$
-            # A = S\beta h_j^2
-            # sol = ddx_{j+1}
-            # b =f-M\ddot{x}_j-Sx_j^*
-            rhs = f - M @ ddu[:, -1] - S @ u_star
-            ddu[:, j + 1] = np.linalg.solve(S * self.beta * self.tau ** 2, rhs)
-            u[:, j + 1] = u_star + self.beta * ddu[:, j + 1] * self.tau ** 2
+            u_star = u[:, j] + self.tau * du[:,j] + (0.5 - self.beta) * self.tau ** 2 * ddu[:, j]
+            du_star = du[:, j] + (1 - self.gamma) * ddu[:,j] * self.tau
 
-        return u, ddu
+            rhs = f[:, j+1] - M @ ddu[:, -1] - S @ u_star
+            ddu[:, j + 1] = np.linalg.solve(S * self.beta * self.tau ** 2 + M, rhs)
+            u[:, j + 1] = u_star + self.beta * ddu[:, j + 1] * self.tau ** 2
+            du[:, j + 1] = du_star + self.gamma * ddu[:, j + 1] * self.tau
+        return u, du, ddu
---
+++ tests/test_newmark.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from src.utils import NewMark
+
+
+def test_newmark():
+    # System parameters
+    M = np.array([[1]])  # Mass matrix
+    S = np.array([[1]])  # Stiffness matrix
+    num_steps = 100
+    tau = 0.1
+    beta = 0.25
+    gamma = 0.5
+
+    # Initial conditions
+    x0 = np.array([1])  # Initial displacement
+    dx0 = np.array([0])  # Initial velocity
+    ddx0 = np.array([0])  # Initial acceleration
+
+    # External force (zero in this case)
+    f = np.zeros((1, num_steps))
+
+    # Create NewMark solver
+    newmark_solver = NewMark(tau, num_steps, beta, gamma)
+
+    # Solve the system
+    u, du, ddu = newmark_solver.solve(M, S, f, x0, dx0, ddx0)
+
+    # Plot results
+    time = np.linspace(0, (num_steps - 1) * tau, num_steps)
+    plt.plot(time, u[0, :], label='Displacement')
+    plt.plot(time, du[0, :], label='Velocity')
+    plt.plot(time, ddu[0, :], label='Acceleration')
+    plt.xlabel('Time')
+    plt.ylabel('Response')
+    plt.legend()
+    plt.title('Newmark Method for SDOF System')
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    test_newmark()
