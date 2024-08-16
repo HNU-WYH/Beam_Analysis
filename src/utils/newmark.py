88c023348ae4a5d96ce7cdcf71a17322dc13b9c3
Specific <yuhan.wu@campus.tu-berlin.de>
2024-08-16 20:15:39 +0200

    adding comment

--- src/utils/newmark.py
+++ src/utils/newmark.py
@@ -2,11 +2,41 @@ import numpy as np
 
 
 class NewMark:
-    '''
-    solving $M\ddot x + S x = f$
-    '''
+    """
+    This class implements the Newmark method to solve a second-order ordinary differential equation (ODE)
+    of the form:
+
+        M*u''(t) + D*u'(t) + S*u(t) = p(t)
+
+    where:
+    - M is the mass matrix,
+    - D is the damping matrix (not explicitly used in this implementation),
+    - S is the stiffness matrix, and
+    - p(t) is the external force vector that can vary with time.
+
+    The Newmark method is an implicit time integration scheme used for solving differential algebraic equations
+    (DAEs), particularly in the context of structural dynamics.
+
+    References:
+    1. Chapter 8 of script1_bending_and_fem.pdf:
+    Discusses the derivation of the DAE using the Galerkin method and
+      weak form. The original PDE is converted into a DAE of the form:
+
+        M_e*x(t) + S_e*x(t) = f_e
+
+    2. Details of the Newmark method can be found in script2_newmark.pdf.
+    """
 
     def __init__(self, tau: float, num_steps: int, beta=0.25, gamma=0.5):
+        """
+        Initialize the NewMark solver with the given parameters.
+
+        Parameters:
+        - tau: Time step size.
+        - num_steps: Number of time steps to simulate.
+        - beta: Newmark parameter, typically 0.25
+        - gamma: Newmark parameter, typically 0.5
+        """
         self.beta = beta
         self.gamma = gamma
         self.tau = tau
