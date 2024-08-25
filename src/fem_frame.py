922dbec384bc8b892b312c9290568660bb5167ea
Specific <yuhan.wu@campus.tu-berlin.de>
2024-08-25 10:51:22 +0200

    frame class

--- src/fem_frame.py
+++ src/fem_frame.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.beam import beam2D
+from src.beam import beam2D, Beam2D
 from src.utils.local_matrix import LocalElement2D
 from src.utils.newmark import NewMark
 from config import LoadType, ConstraintType, SolvType
@@ -11,4 +11,20 @@ class FrameworkFEM:
     def __init__(self):
         pass
 
-    def
+    def add_beam(self, beam: Beam2D, joint_coordinate, connection_type):
+        pass
+
+    def apply_force(self):
+        pass
+
+    def add_constraint(self):
+        pass
+
+    def visualize(self):
+        pass
+
+    def assemble_frame_matrices(self):
+        pass
+
+    def solve(self):
+        pass
